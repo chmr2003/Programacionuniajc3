199f794ddb48eff6076553fcc611bff44120484d
chmr2003 <chmendoza@estudiante.uniajc.edu.co>
2025-02-04 17:05:13 +0000

    Modificacion System

--- Ejecutar.java
+++ Ejecutar.java
@@ -1,6 +1,5 @@
 public class Ejecutar {
-
-    public static void main (string args) {
-        system.out.println("Hola mundo UNIAJC") ;
+    public static void main(String[] args) {
+        System.out.println("Hola mundo UNIAJC");
     }
 }
